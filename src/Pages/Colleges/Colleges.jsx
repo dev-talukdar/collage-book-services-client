47977068ba5558ce51cb976fa536111e97fbfd3a
dev-talukdar <dev.talukdaar@gmail.com>
2023-07-24 19:34:28 +0600

    collage card is now process

--- src/Pages/Colleges/Colleges.jsx
+++ src/Pages/Colleges/Colleges.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import CollegeCard from "./CollegeCard";
+import { useEffect } from "react";
+
  
 
 const Colleges = () => {
+     const [collage, setCollage] = useState([]);
+     const [showAllCollage, setShowAllCollage] = useState(false);
+
+     useEffect(() => {
+        fetch('data.json')
+        .then(res=> res.json())
+        .then(data=> setCollage(data))
+     }, [])
+
+     const displayCollage = showAllCollage ? collage : collage.slice (0,6)
+
+     const handleViewMore = () => {
+        setShowAllCollage(true);
+
+     }
+
     return (
         <div>
-            <h2>this is my colleges page</h2>
+            <CollegeCard></CollegeCard>
         </div>
     );
 };
